91b80a8f2908995ba188e431027939b270218aa5
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2010-12-28 16:49:51 -0700

    Added Koans on how to create stackable traits, and how the diamond of death is avoided.

--- src/test/scala/org/functionalkoans/forscala/AboutTraits.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTraits.scala
@@ -112,4 +112,126 @@ class AboutTraits extends KoanSuite with ShouldMatchers {
     welder.logCache.size should be(1)
     baker.logCache.size should be(1)
   }
+
+  koan("Traits are instantiated before a classes instantition") {
+    var sb = List[String]()
+
+    trait T1 {
+      sb = sb :+ ("In T1: x=%s".format(x))
+      val x = 1
+      sb = sb :+ ("In T1: x=%s".format(x))
+    }
+
+    class C1 extends T1 {
+      sb = sb :+ ("In C1: y=%s".format(y))
+      val y = 2
+      sb = sb :+ ("In C1: y=%s".format(y))
+    }
+
+    sb = sb :+ ("Creating C1")
+    new C1
+    sb = sb :+ ("Created C1")
+
+    sb.mkString(";") should be("Creating C1;In T1: x=0;In T1: x=1;In C1: y=0;In C1: y=2;Created C1")
+  }
+
+
+  koan("Traits are instantiated before a classes instantition from left to right") {
+    var sb = List[String]()
+
+    trait T1 {
+      sb = sb :+ ("In T1: x=%s".format(x))
+      val x = 1
+      sb = sb :+ ("In T1: x=%s".format(x))
+    }
+
+    trait T2 {
+      sb = sb :+ ("In T2: z=%s".format(z))
+      val z = 1
+      sb = sb :+ ("In T2: z=%s".format(z))
+    }
+
+    class C1 extends T1 with T2 {
+      sb = sb :+ ("In C1: y=%s".format(y))
+      val y = 2
+      sb = sb :+ ("In C1: y=%s".format(y))
+    }
+
+    sb = sb :+ ("Creating C1")
+    new C1
+    sb = sb :+ ("Created C1")
+
+    sb.mkString(";") should be(
+      "Creating C1;In T1: x=0;In T1: x=1;In T2: z=0;In T2: z=1;In C1: y=0;In C1: y=2;Created C1")
+  }
+
+  koan("Instantiations are tracked and will not allow a duplicate instantiation. " +
+          "Note T1 extends T2, and C1 also extends T2, but T2 is only instantiated twice.") {
+
+    var sb = List[String]()
+
+    trait T1 extends T2 {
+      sb = sb :+ ("In T1: x=%s".format(x))
+      val x = 1
+      sb = sb :+ ("In T1: x=%s".format(x))
+    }
+
+    trait T2 {
+      sb = sb :+ ("In T2: z=%s".format(z))
+      val z = 1
+      sb = sb :+ ("In T2: z=%s".format(z))
+    }
+
+    class C1 extends T1 with T2 {
+      sb = sb :+ ("In C1: y=%s".format(y))
+      val y = 2
+      sb = sb :+ ("In C1: y=%s".format(y))
+    }
+
+    sb = sb :+ ("Creating C1")
+    new C1
+    sb = sb :+ ("Created C1")
+
+    sb.mkString(";") should
+            be("Creating C1;In T2: z=0;In T2: z=1;In T1: x=0;In T1: x=1;In C1: y=0;In C1: y=2;Created C1")
+  }
+
+
+  koan("The diamond of death (http://en.wikipedia.org/wiki/Diamond_problem) is avoided since " +
+          "instantiations are tracked and will not allow multiple instantiations") {
+
+    var sb = List[String]()
+
+    trait T1 {
+      sb = sb :+ ("In T1: x=%s".format(x))
+      val x = 1
+      sb = sb :+ ("In T1: x=%s".format(x))
+    }
+
+    trait T2 extends T1 {
+      sb = sb :+ ("In T2: z=%s".format(z))
+      val z = 2
+      sb = sb :+ ("In T2: z=%s".format(z))
+    }
+
+    trait T3 extends T1 {
+      sb = sb :+ ("In T3: w=%s".format(w))
+      val w = 3
+      sb = sb :+ ("In T3: w=%s".format(w))
+    }
+
+    class C1 extends T2 with T3 {
+      sb = sb :+ ("In C1: y=%s".format(y))
+      val y = 4
+      sb = sb :+ ("In C1: y=%s".format(y))
+    }
+
+    sb = sb :+ ("Creating C1")
+    new C1
+    sb = sb :+ ("Created C1")
+
+    sb.mkString(";") should be
+            ("Creating C1;In T1: x=0;In T1: x=1;In T2: z=0;In T2: z=2;" +
+                    "In T3: w=0;In T3: w=3;In C1: y=0;In C1: y=4;Created C1")
+  }
 }
