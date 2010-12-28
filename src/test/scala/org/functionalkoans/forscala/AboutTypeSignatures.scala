cf580fb7b215de12f2491af527ead1a338da9bb6
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2010-12-28 13:41:38 -0700

    Added class def for AboutTypeSignatures, but no koans added yet.

---
+++ src/test/scala/org/functionalkoans/forscala/AboutTypeSignatures.scala
@@ -0,0 +1,21 @@
+package org.functionalkoans.forscala
+
+import org.scalatest.matchers.ShouldMatchers
+import support.KoanSuite
+
+/**
+ *
+ * This should come after traits
+ *
+ * Created by Daniel Hinojosa
+ * User: Daniel Hinojosa
+ * Date: 12/21/10
+ * Time: 8:49 PM
+ * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
+ * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
+ * tel: 505.363.5832
+ */
+class AboutTypeSignatures extends KoanSuite with ShouldMatchers {
+
+
+}
