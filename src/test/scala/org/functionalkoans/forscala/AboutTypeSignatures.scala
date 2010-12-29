91e5168d2e33d5a5e2b3bc1ef636da1d7f2df4a7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2010-12-28 22:28:16 -0700

    Added Type Signature Koans, basic koans, inheritance, casting, asInstanceOf.

--- src/test/scala/org/functionalkoans/forscala/AboutTypeSignatures.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTypeSignatures.scala
@@ -2,7 +2,6 @@ package org.functionalkoans.forscala
 
 import org.scalatest.matchers.ShouldMatchers
 import support.KoanSuite
-
 /**
  *
  * This should come after traits
@@ -15,7 +14,95 @@ import support.KoanSuite
  * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
  * tel: 505.363.5832
  */
+
 class AboutTypeSignatures extends KoanSuite with ShouldMatchers {
+  koan("In Java you declare a generic type within a <>, in Scala it is []") {
+    val z: List[String] = "Do" :: "Re" :: "Mi" :: "Fa" :: "So" :: "La" :: "Te" :: "Do" :: Nil
+  }
 
+  koan("Most of the time, Scala will infer the type and [] are optional") {
+    val z = "Do" :: "Re" :: "Mi" :: "Fa" :: "So" :: "La" :: "Te" :: "Do" ::
+            Nil //Infers that the list assigned to variable is of type List[String]
+  }
 
+  koan("A trait can be declared containing a type, where a concrete implmenter will satisfy the type") {
+    trait Randomizer[A] {
+      def draw(): A
+    }
+
+    class IntRandomizer extends Randomizer[Int] {
+      def draw = {
+        import util.Random
+        Random.nextInt()
+      }
+    }
+
+    val intRand = new IntRandomizer
+    intRand.draw should be < Int.MaxValue
+  }
+
+  koan("Class meta-information can be retrieved by class name by using classOf[className]") {
+    classOf[String].getCanonicalName() should be("java.lang.String")
+    classOf[String].getSimpleName() should be("String")
+  }
+
+  koan("Class meta-information can be derived from an object reference using getClass()") {
+    val zoom = "zoom"
+    zoom.getClass should be(classOf[String])
+    zoom.getClass.getCanonicalName() should be("java.lang.String")
+    zoom.getClass.getSimpleName() should be("String")
+  }
+
+  koan("isInstanceOf[className] is used to determine the if an object reference is an instance of given class") {
+    trait Randomizer[A] {
+      def draw(): A
+    }
+
+    class IntRandomizer extends Randomizer[Int] {
+      def draw = {
+        import util.Random
+        Random.nextInt()
+      }
+    }
+
+    val intRand = new IntRandomizer
+    intRand.draw.isInstanceOf[Int] should be(true)
+  }
+
+  koan("asInstanceOf[className] is used to cast one reference to another") {
+    trait Randomizer[A] {
+      def draw(): A
+    }
+
+    class IntRandomizer extends Randomizer[Int] {
+      def draw = {
+        import util.Random
+        Random.nextInt()
+      }
+    }
+
+    val intRand = new IntRandomizer
+    val rand = intRand
+    val intRand2 = rand.asInstanceOf[IntRandomizer]
+  }
+
+  koan("asInstanceOf[className] will throw a ClassCastException if a class derived from " +
+          "and the class target aren't from the same inheritance branch") {
+    trait Randomizer[A] {
+      def draw(): A
+    }
+
+    class IntRandomizer extends Randomizer[Int] {
+      def draw = {
+        import util.Random
+        Random.nextInt()
+      }
+    }
+
+    val intRand = new IntRandomizer
+
+    intercept[ClassCastException] {
+      intRand.asInstanceOf[String]  //intRand cannot be cast to String
+    }
+  }
 }
