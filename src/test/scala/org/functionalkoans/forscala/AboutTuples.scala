522c241be9d6352050e82292f13b1dd700a7eded
dmarsh <dmarsh@srtsolutions.com>
2011-02-21 17:09:50 -0500

    use isInstanceOf not just values in tests

--- src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
@@ -9,7 +9,7 @@ class AboutTuples extends KoanSuite with ShouldMatchers {
 
   koan ("Tuples can be created easily") {
     val tuple = ("apple", "dog")
-    tuple should be (("apple", "dog"))
+    tuple should be ("apple", "dog")
   }
 
   koan ("Tuple items may be accessed individually") {
@@ -24,11 +24,11 @@ class AboutTuples extends KoanSuite with ShouldMatchers {
   koan ("Tuples may be of mixed type") {
     val tuple5 = ("a", 1, 2.2, new Date(), BigDecimal(5))
 
-    tuple5._1 should be ("a")
-    tuple5._2 should be (1)
+    tuple5._1.isInstanceOf[String]
+    tuple5._2.isInstanceOf[Number]
     tuple5._3 should be (2.2)
-    tuple5._4.before(new Date()) should be (true)
-    tuple5._5 should be (BigDecimal(5))
+    tuple5._4.isInstanceOf[Date]
+    tuple5._5 should be (5)
   }
 
   
