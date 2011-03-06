41573877cbd45403bbe2fd0f10bed7e23f218b9e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-03-05 22:46:32 -0700

    Fixed error with AboutNamedAndDefaultArguments since they shouldn't have been there in this repository.

--- src/test/scala/org/functionalkoans/forscala/AboutNamedAndDefaultArguments.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutNamedAndDefaultArguments.scala
@@ -77,17 +77,12 @@ class AboutNamedAndDefaultArguments() extends KoanSuite with ShouldMatchers {
 
     myColor should equal(0, 325, 100)
   }
+
   koan("default parameters can be functional too") {
     def reduce(a: Int, f: (Int, Int) => Int = (_ + _)): Int = f(a, a)
 
-    reduce(5) should equal(__)
-    reduce(5, _ * _) should equal(__)
+    reduce(5) should equal(10)
+    reduce(5, _ * _) should equal(25)
   }
-
-
 }
 
-
-
-
-	
