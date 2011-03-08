b59598c3b34dbadbffbeac8afc2e24c34d6ae5ea
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-03-07 19:00:59 -0700

    Added a class manifests to AboutManifests.

--- src/test/scala/org/functionalkoans/forscala/AboutManifests.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutManifests.scala
@@ -12,11 +12,24 @@ import support.KoanSuite
  * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
  * tel: 505.363.5832
  */
+
+class Monkey
+
+
 class AboutManifests extends KoanSuite with ShouldMatchers {
   koan("""Manifests can be used to determine a type used
          |   before it erased by the VM by using an implicit manifest argument.""") {
     def inspect[T](l: List[T])(implicit manifest: scala.reflect.Manifest[T]) = manifest.toString
     val list = 1 :: 2 :: 3 :: 4 :: 5 :: Nil
     inspect(list) should be("Int")
+  }
+
+  koan("""Manifests can be attached to classes. Manifests have other meta-information about
+          |  the type erased""") {
+    class Barrel[T](implicit m: scala.reflect.Manifest[T]) {
+      def +(t: T) = "1 %s has been added".format(m.erasure.getSimpleName) //Simple-name of the class erased
+    }
+    val monkeyBarrel = new Barrel[Monkey]
+    (monkeyBarrel + new Monkey) should be ("1 Monkey has been added")
   }
 }
