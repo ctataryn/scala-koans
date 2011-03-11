ed259532df4c72230d89a56618fd9affda93e511
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-03-11 12:34:59 -0700

    Added Implicit conversion technique, and implicit function parameters.

--- src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
@@ -3,6 +3,7 @@ package org.functionalkoans.forscala
 import org.scalatest.matchers.ShouldMatchers
 import support.KoanSuite
 
+
 /**
  * Created by Daniel Hinojosa
  * User: Daniel Hinojosa
@@ -30,16 +31,43 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
 
   koan("""Implicits rules can be imported into your scope with an import""") {
     object MyPredef {
+
       class KoanIntWrapper(val original: Int) {
         def isOdd() = original % 2 != 0
 
         def isEven() = !isOdd()
       }
+
       implicit def thisMethodNameIsIrrelevant(value: Int) = new KoanIntWrapper(value)
     }
-    import MyPredef._ //imported implicits come into effect with in this scope
+
+    import MyPredef._
+    //imported implicits come into effect within this scope
     19.isOdd() should be(true)
     20.isOdd() should be(false)
+  }
+
+  koan("""Implicits can be used to automatically convert one type to another""") {
+
+    import java.math.BigInteger
+    implicit def Int2BigIntegerConvert(value: Int): BigInteger = new BigInteger(value.toString)
+
+    def add(a: BigInteger, b: BigInteger) = a.add(b)
+
+    (add(3, 6)) should be(new BigInteger("9"))
+  }
+
+  koan("""Implicits can be used declare a value to be provided as a default as
+          |   long as an implicit value is set with in the scope.  These are
+          |   called implicit function parameters""") {
+
+    def howMuchCanIMake_?(hours:Int)(implicit dollarsPerHour:BigDecimal) = dollarsPerHour * hours
+
+    implicit var hourlyRate = BigDecimal(34.00)
+    howMuchCanIMake_?(30) should be (1020.00)
+
+    hourlyRate = BigDecimal(95.00)
+    howMuchCanIMake_?(95) should be (9025.00)
   }
 
 
