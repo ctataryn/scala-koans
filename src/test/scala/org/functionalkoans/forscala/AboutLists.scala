f38df9ab0f0ac35737270148e7fa002ed1af71f3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-03-14 11:34:12 -0600

    Added 'until' in Range Koan.

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -108,6 +108,8 @@ class AboutLists extends KoanSuite with ShouldMatchers {
   koan("You can create a list from a range") {
     val a = (1 to 5).toList
     a should be(List(1, 2, 3, 4, 5))
+    val b = (1 until 5).toList
+    b should be(List(1, 2, 3, 4))
   }
 
   koan("Lists reuse their tails") {
