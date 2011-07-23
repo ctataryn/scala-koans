a5325d6be40fa0f2c2ecc90b73af7c03470f5e1c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-07-23 16:52:16 -0600

    Removed .isInstanceOf[Number] since from what I can tell there is no longer a Number class in Scala

--- src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
@@ -25,7 +25,7 @@ class AboutTuples extends KoanSuite with ShouldMatchers {
     val tuple5 = ("a", 1, 2.2, new Date(), BigDecimal(5))
 
     tuple5._1.isInstanceOf[String]
-    tuple5._2.isInstanceOf[Number]
+    tuple5._2.isInstanceOf[Int] 
     tuple5._3 should be(2.2)
     tuple5._4.isInstanceOf[Date]
     tuple5._5 should be(5)
