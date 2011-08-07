1cefe75ea5c5d2aa3d2abf2a416c2be71bc95394
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-08-07 01:01:23 -0600

    Added more to AboutTraversables, an advanced look at method available to most collections/streams

--- src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
@@ -9,18 +9,68 @@ class AboutTraversables extends KoanSuite with ShouldMatchers {
           |   The methods involved can be applied to each other in a different type.  ++ appends 
           |   two Traversables together.""") {
 
-         val set = Set(1, 9, 10, 22)
-         val list = List(3, 4, 5, 10)
-         val result = set ++ list
-         result.size should be (7)
+    val set = Set(1, 9, 10, 22)
+    val list = List(3, 4, 5, 10)
+    val result = set ++ list
+    result.size should be(7)
 
-         val result2 = list ++ set
-         result2.size should be (8)
+    val result2 = list ++ set
+    result2.size should be(8)
   }
 
-  koan("""Map will apply the given function on all elements of a Traversable""") { 
-         val set = Set(1, 3, 4, 6)
-         val result = set.map(_ * 4)
-         result.last should be (24)
+  koan("""map will apply the given function on all elements of a
+          |  Traversable and return a new collection of the result.""") {
+    val set = Set(1, 3, 4, 6)
+    val result = set.map(_ * 4)
+    result.last should be(24)
+  }
+
+  koan("""flatten will smash all child Traversables within a Traversable""") {
+    val list = List(List(1), List(2, 3, 4), List(5, 6, 7), List(8, 9, 10))
+    list.flatten should be(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+  }
+
+  koan("""flatMap will not only apply the given function on all elements of a Traversable,
+          |  but all elements within the elements and flatten the results""") {
+    val list = List(List(1), List(2, 3, 4), List(5, 6, 7), List(8, 9, 10))
+    val result = list.flatMap(_.map(_ * 4))
+    result should be(List(4, 8, 12, 16, 20, 24, 28, 32, 36, 40))
+  }
+
+  koan("""flatMap of Options will filter out all Nones and Keep the Somes""") {
+    val list = List(1, 2, 3, 4, 5)
+    val result = list.flatMap(it => if (it % 2 == 0) Some(it) else None)
+    result should be (List(2, 4))
+  }
+
+  koan("""collect will apply a partial function to all elements of a Traversable
+          and will return a different collection. In this koan, a case fragment is a partial function.""") {
+    val list = List(4, 6, 7, 8, 9, 13, 14)
+    val result = list.collect {
+      case x: Int if (x % 2 == 0) => x * 3
+    }
+    result should be (List(12, 18, 24, 42))
+  }
+
+  koan("""collect will apply a partial function to all elements of a Traversable
+          |  and will return a different collection. In this koan, two case fragments are chained to create
+          |  a more robust result.""") {
+    val list = List(4, 6, 7, 8, 9, 13, 14)
+    val partialFunction1: PartialFunction[Int, Int] = {
+      case x: Int if (x % 2 == 0) => x * 3
+    }
+    val partialFunction2: PartialFunction[Int, Int] = {
+      case y: Int if (y % 2 != 0) => y * 4
+    }
+    val result = list.collect(partialFunction1 orElse partialFunction2)
+    result should be(List(12, 18, 28, 24, 36, 52, 42))
+  }
+
+  koan("""foreach will apply a function to all elements of a Traversable, but unlike
+          | the map function, it will not return anything since the return type is Unit, which
+          | is like a void return type in Java, C++""") {
+    val list = List(4, 6, 7, 8, 9, 13, 14)
+    list.foreach(num => println(num * 4))
+    list should be(List(4, 6, 7, 8, 9, 13, 14))
   }
 }
