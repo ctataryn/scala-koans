445b012fee59d0e86a8c9306c019ef830e6b94b0
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-08-17 10:44:16 -0600

    Added transpose.

--- src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
@@ -373,5 +373,16 @@ class AboutTraversables extends KoanSuite with ShouldMatchers {
     result should be(6)
   }
 
-  //TODO:Transpose, withFilter
+  koan("""`transpose` will take a traversable of traversables and group them by their position in
+           |  it's own traversable.  E.g. ((x1, x2),(y1, y2)).transpose = (x1, y1), (x2, y2).
+           |  or ((x1, x2),(y1, y2),(z1, z2)).transpose = ((x1, y1, z1), (x2, y2, z2), (x3, y3, z3))""") {
+    val list = List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))
+    list.transpose should be(List(List(1, 4, 7), List(2, 5, 8), List(3, 6, 9)))
+
+
+    val list2 = List(List(1), List(4))
+    list2.transpose should be(List(List(1, 4)))
+  }
+
+  //TODO: withFilter
 }
